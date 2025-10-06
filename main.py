7abb7ae646ff64f34e1e8ce863710150c6e09edf
Rob L <viewpl@gmail.com>
2025-10-07 10:50:56 +1100

    fix: load from all files at first

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ print('{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}'.for
     'sell_transaction.volume', 'sell_transaction.datetime',
     'sell_transaction.operation', 'sell_transaction.pair',
     'sell_transaction.usd'))
+
+parsed_trans = []
 for item in sys.argv[1:]:
   with open(item, 'r') as csvfile:
     csvcontent = csv.reader(csvfile, delimiter=',', quotechar='"')
-    parsed_trans = []
     attrs = None
     for index, row in enumerate(csvcontent):
       if index == 0:
@@ -44,23 +45,25 @@ for item in sys.argv[1:]:
           parsed_trans.append(current_trans)
         else:
           continue
-    for tran in parsed_trans:
-      statement = statements_dict.get(tran.financial_year)
-      if statement:
-        statement.process_transaction(tran)
-      else:
-        # new financial year, create new statement
-        previous_statement = statements[-1][1] if len(statements) else None
-        previous_portfolio = copy.deepcopy(
-            previous_statement.portfolio) if previous_statement else None
-        statement = AnnualStatement(
-            financial_year=tran.financial_year,
-            portfolio=previous_portfolio,
-            losses=previous_statement.carried_losses
-            if previous_statement else None)
-        statement.process_transaction(tran)
-        statements.append((tran.financial_year, statement))
-        statements_dict[tran.financial_year] = statement
+  
+parsed_trans.sort(key=lambda x: x.datetime) # sort by datetime ascending
+for tran in parsed_trans:
+  statement = statements_dict.get(tran.financial_year)
+  if statement:
+    statement.process_transaction(tran)
+  else:
+    # new financial year, create new statement
+    previous_statement = statements[-1][1] if len(statements) else None
+    previous_portfolio = copy.deepcopy(
+        previous_statement.portfolio) if previous_statement else None
+    statement = AnnualStatement(
+        financial_year=tran.financial_year,
+        portfolio=previous_portfolio,
+        losses=previous_statement.carried_losses
+        if previous_statement else None)
+    statement.process_transaction(tran)
+    statements.append((tran.financial_year, statement))
+    statements_dict[tran.financial_year] = statement
 
 for item in statements:
   item[1].report()
